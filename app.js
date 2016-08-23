2fa8dc5bd4e5b3e38e59e9f4502c60ad46c957a2
JikwangKim <comsamo84@gmail.com>
2016-08-23 14:34:19 +0900

    포트변경

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4041;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
